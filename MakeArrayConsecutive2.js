83b05aa23431fa492906cbd6f189033d7ccfd242
Abnetwolde <tecnoabi4@gmail.com>
2023-06-09 02:48:44 -0700

    missing numbers

--- MakeArrayConsecutive2.js
+++ MakeArrayConsecutive2.js
@@ -6,5 +6,13 @@ console.log(result)
 return max-min-array.length+1;
 
 }
-
+function MissingNumber(max,min,array){
+    let missingStatues = [];
+    for (let i = min + 1; i < max; i++) {
+        if (!array.includes(i)) {
+            missingStatues.push(i);
+        }
+    }
+    return missingStatues;
+}
 console.log(MakeArrayConsecutive2([3,5,7,9]))
