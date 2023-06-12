a76ddea891ecf94c95d01e8b8c10492a110a3e46
Abnetwolde <tecnoabi4@gmail.com>
2023-06-12 15:53:10 -0700

    AllLongestStrings

---
+++ Level_3/AllLongestStrings.js
@@ -0,0 +1,21 @@
+/* Given an array of strings, return another array containing all of its longest strings.
+
+Example
+
+For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+solution(inputArray) = ["aba", "vcd", "aba"] */
+
+function solution(array) {
+  let longest = []
+  const sortArray = array.sort((a, b) => b.length - a.length);
+  let count=sortArray[0].length;
+  for (let index = 0; index < sortArray.length; index++) {
+        if(count===sortArray[index].length){
+          longest.push(sortArray[index])
+        }
+  }
+  console.log(count)
+  console.log(sortArray)
+  return longest
+}
+console.log(solution(["aba", "aa", "ad", "vcd", "aba"]))
