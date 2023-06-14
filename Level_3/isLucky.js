b7f5689495a5e64ca72b2b5bf234c54f83a08403
Abnetwolde <tecnoabi4@gmail.com>
2023-06-14 04:34:55 -0700

    isLucky2

--- Level_3/isLucky.js
+++ Level_3/isLucky.js
@@ -43,4 +43,11 @@ function isLucky(num){
     return sum1 == sum2;
 }
  */
+
+/* function solution(n) {
+    var count = 0;
+    n = String(n).split('').map(t => {return parseInt(t)});
+    n.forEach( (el, i) => { (i < n.length / 2) ? count += el : count -= el });
+    return count == 0;
+} */
 console.log(isLucky(7318))
