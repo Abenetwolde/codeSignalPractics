affb7459f5f4663b25f248af16e8e818a0e4a144
Abnetwolde <tecnoabi4@gmail.com>
2023-06-15 07:17:11 -0700

    reverseInParentheses

--- Level_3/reverseInParentheses.js
+++ Level_3/reverseInParentheses.js
@@ -1,13 +1,22 @@
 function reverseInParentheses(inputString){
-let stringArray=inputString.split("").reverse()
-for (let index = 0; index < stringArray.length; index++) {
-    if(stringArray[index]=="("||")"){
-        const element = stringArray[index];
-        console.log(element)
-        // stringArray.splice(indexOf(stringArray[index]) ,1)
+    const closeIndex = inputString.indexOf(')');
+  
+    // If there is no closing parenthesis, return the input string
+    if (closeIndex === -1) {
+      return inputString;
     }
     
-}
-return stringArray
+    // Find the last opening parenthesis before the first closing parenthesis
+    const openIndex = inputString.substring(0, closeIndex).lastIndexOf('(');
+    
+    // Reverse the characters between the opening and closing parentheses
+    const start = inputString.substring(0, openIndex);
+    const middle = inputString.substring(openIndex + 1, closeIndex).split('').reverse().join('');
+    const end = inputString.substring(closeIndex + 1);
+    
+    // Recursively call the function with the updated string
+    return reverseInParentheses(start + middle + end);
+  
+  
 }
 console.log(reverseInParentheses("foo(bar)baz"))
