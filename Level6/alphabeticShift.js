8d740b78ba65633f332d845e162da10bbab20fe2
Abnetwolde <tecnoabi4@gmail.com>
2023-07-18 01:16:39 -0700

    alphabeticShift

---
+++ Level6/alphabeticShift.js
@@ -0,0 +1,17 @@
+function alphabeticShift(inputString) {
+    let result = "";
+    for (let i = 0; i < inputString.length; i++) {
+        let charCode = inputString.charCodeAt(i);
+        if (charCode === 122) { // 'z'
+            result += 'a';
+        } else if (charCode === 90) { // 'Z'
+            result += 'A';
+        } else {
+            result += String.fromCharCode(charCode + 1);
+        }
+    }
+    return result;
+}
+console.log(alphabeticShift('jvdfgdfz'))
+// This function uses the charCodeAt method to get the Unicode value of each character in the input string. It then checks if the character is ‘z’ or ‘Z’ and replaces it with ‘a’ or ‘A’, respectively. Otherwise, it increments the Unicode value by 1 and converts it back to a character using the fromCharCode method. 
+// The resulting characters are concatenated to form the output string.
