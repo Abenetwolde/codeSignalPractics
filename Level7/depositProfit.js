6f1516a2f570e279e4b7c866aba6a092e99e859e
Abnetwolde <tecnoabi4@gmail.com>
2023-07-18 02:45:32 -0700

    depositProfit

--- Level7/depositProfit.js
+++ Level7/depositProfit.js
@@ -13,5 +13,12 @@ function alphabeticShift(inputString) {
     return result;
 }
 console.log(alphabeticShift('jvdfgdfz'))
+// const solution = inputString =>
+//     inputString.split('').map(el => 
+//         (el === 'z') ? 'a' : String.fromCharCode(el.charCodeAt() + 1)
+//     ).join('')
+
+
+
 // This function uses the charCodeAt method to get the Unicode value of each character in the input string. It then checks if the character is ‘z’ or ‘Z’ and replaces it with ‘a’ or ‘A’, respectively. Otherwise, it increments the Unicode value by 1 and converts it back to a character using the fromCharCode method. 
 // The resulting characters are concatenated to form the output string.
